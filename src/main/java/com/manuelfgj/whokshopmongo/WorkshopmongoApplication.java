693d16fc9053193af8bfc090fd32b2c125984fa9
Manuel Gomes <manuel.fgj@gmail.com>
2020-05-08 16:54:54 -0300

    Primeiro commit - projeto criado

---
+++ src/main/java/com/manuelfgj/whokshopmongo/WorkshopmongoApplication.java
@@ -0,0 +1,13 @@
+package com.manuelfgj.whokshopmongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshopmongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkshopmongoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/manuelfgj/whokshopmongo/WorkshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manuelfgj.whokshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
